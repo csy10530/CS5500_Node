25902586d63344d687592565e7df91a83ea7029a
csy10530 <chen.sy909@gmail.com>
2022-03-30 20:01:52 -0400

    fixed server bugs

--- server.ts
+++ server.ts
@@ -1,45 +1,74 @@
 /**
- * @file Implements an Express HTTP server using Node.
- * Declares RESTful Web Services enabling CRUD operations on the following resources:
+ * @file Implements an Express Node HTTP server. Declares RESTful Web services
+ * enabling CRUD operations on the following resources:
  * <ul>
- *     <li>tuits</li>
  *     <li>users</li>
+ *     <li>tuits</li>
  *     <li>likes</li>
- *     <li>bookmarks</li>
- *     <li>follows</li>
- *     <li>messages</li>
  * </ul>
+ *
+ * Connects to a remote MongoDB instance hosted on the Atlas cloud database
+ * service
  */
 import express, {Request, Response} from 'express';
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
-
-import TuitController from "./controllers/TuitController";
 import UserController from "./controllers/UserController";
+import TuitController from "./controllers/TuitController";
 import LikeController from "./controllers/LikeController";
-import FollowController from "./controllers/FollowController";
-import BookmarkController from "./controllers/BookmarkController";
-import MessageController from "./controllers/MessageController";
+//import SessionController from "./controllers/SessionController";
+//import AuthenticationController from "./controllers/AuthenticationController";
+import mongoose from "mongoose";
+//import GroupController from "./controllers/GroupController";
+import bodyParser from "body-parser";
+const cors = require("cors");
+const session = require("express-session");
 
-const cors = require('cors');
+// build the connection string
+const PROTOCOL = "mongodb+srv";
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const HOST = "cs5500.t59xq.mongodb.net";
+const DB_NAME = "tuiter";
+const DB_QUERY = "retryWrites=true&w=majority";
+const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
+mongoose.connect(connectionString);
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CORS_ORIGIN
+}));
 
-mongoose.connect('mongodb+srv://SiyuanChen:CS5500Sp22@cs5500.t59xq.mongodb.net/tuiter?retryWrites=true&w=majority');
+let sess = {
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    saveUninitialized: true,
+    resave: true,
+    cookie: {
+        sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax',
+        secure: process.env.ENV
+    }
+}
 
-app.get('/', (req:Request, res:Response) => res.send('Welcome!'));
-app.get('/hello', (req: Request, res: Response) => res.send('Hello World!'));
+if (process.env.NODE_ENV === 'production') {
+    app.set('trust proxy', 1) // trust first proxy
+}
 
-// create RESTful Web service controllers
+app.use(session(sess))
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) =>
+    res.send('Welcome!'));
+
+// create RESTful Web service API
 const userController = UserController.getInstance(app);
 const tuitController = TuitController.getInstance(app);
-const likeController = LikeController.getInstance(app);
-const followController = FollowController.getInstance(app);
-const bookmarkController = BookmarkController.getInstance(app);
-const messageController = MessageController.getInstance(app);
-
-// Start a server that listens at port 4000 or an available environment port
+const likesController = LikeController.getInstance(app);
+//SessionController(app);
+//AuthenticationController(app);
+//GroupController(app);
+/**
+ * Start a server listening at port 4000 locally
+ * but use environment variable PORT on Heroku if available.
+ */
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
