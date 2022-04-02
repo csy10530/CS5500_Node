242b522c07e0804029c8324d0d90c61b879ee471
csy10530 <chen.sy909@gmail.com>
2022-04-02 13:09:20 -0400

    modified env settings

--- server.ts
+++ server.ts
@@ -44,12 +44,12 @@ let sess = {
     saveUninitialized: true,
     resave: true,
     cookie: {
-        sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax',
-        secure: process.env.NODE_ENV === "production"
+        sameSite: process.env.NODE_ENV === "PRODUCTION" ? 'none' : 'lax',
+        secure: process.env.NODE_ENV
     }
 }
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'PRODUCTION') {
     app.set('trust proxy', 1) // trust first proxy
 }
 
